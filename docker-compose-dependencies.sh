67b1e0b7e916a26d977fa1fd1ad0a6496b3f55ba
AzureAD\HuiningLi <huining.li@fsoft.net.au>
2021-11-24 11:59:45 +1000

    remove sudo

--- docker-compose-dependencies.sh
+++ docker-compose-dependencies.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 docker-compose -v
